de3832a48981de76ea0a83acbac2f7b799b2f080
znhoughton <77252741+znhoughton@users.noreply.github.com>
2022-01-19 01:17:20 -0800

    Update 2Gram Odds Ratio Write CSV (POS unspecified).py

--- Google NGrams/2Gram Odds Ratio Write CSV (POS unspecified).py
+++ Google NGrams/2Gram Odds Ratio Write CSV (POS unspecified).py
@@ -4,42 +4,44 @@
 
 ###POS Unspecified was used for the staub replication
 
+###See https://vorpus.org/papers/draft/zs-paper.pdf for the full paper on the ZS file format
+
 if __name__ == '__main__':
 
     from zs import ZS
     import math
     import csv
 
-    z = ZS("D:\PhD Stuff\Linguistics Stuff\Google NGRAMS\google-books-eng-us-all-20120701-2gram.zs")
-    z_1gram = ZS("D:\PhD Stuff\Linguistics Stuff\Google NGRAMS\google-books-eng-us-all-20120701-1gram.zs")
+    z = ZS("D:\PhD Stuff\Linguistics Stuff\Google NGRAMS\google-books-eng-us-all-20120701-2gram.zs") #bigram corpus
+    z_1gram = ZS("D:\PhD Stuff\Linguistics Stuff\Google NGRAMS\google-books-eng-us-all-20120701-1gram.zs") #onegram corpus
 
     onegram_memory = {}
     corpus_size = 0
     for i, item in enumerate (z_1gram.search()):
 
-        one_gram_string = item.decode('utf-8')
-        one_gram_output = one_gram_string.split('\t')
-        one_gram_match_count = one_gram_output[2]
-        one_gram_volume_count = one_gram_output[3]
+        one_gram_string = item.decode('utf-8') #decode string
+        one_gram_output = one_gram_string.split('\t') #split by tab
+        one_gram_match_count = one_gram_output[2] #match count
+        one_gram_volume_count = one_gram_output[3] #volume count
         word = one_gram_output[0]
         word = word.lower()
         year = one_gram_output[1]
                     
 
-        if i % 10000000 == 0:
+        if i % 10000000 == 0: #for debugging purposes
             print(one_gram_string)
             print(word)
 
-        if len(word.split('_')) == 2:
+        if len(word.split('_')) == 2: #length of rows with part of speech tagging are different than length of rows without, this line accounts for that and splits by POS.
             word, POS = word.split('_')
 
         else:
             word = word
 
-        if word.isalpha():
-            corpus_size = corpus_size + int(one_gram_match_count) 
+        if word.isalpha(): #ignore non-words
+            corpus_size = corpus_size + int(one_gram_match_count) #keep track of corpus size
 
-    def two_gram_odds_ratio(word1, word2):
+    def two_gram_odds_ratio(word1, word2): #function for getting the odds ratio for any two words
         
         if __name__ == '__main__':
         
@@ -53,13 +55,13 @@ if __name__ == '__main__':
             two_gram_two = 0
             for record in z.search(start=word1 + b" " + word2 + b"\t1980", stop=word1 + b" " + word2 + b"\t2009"): #search for each 2gram between 1980 and 2009
                 navy_ship = record.decode("utf-8")
-                n_gram = navy_ship.split("\t")#separate the string so we can focus on what we care about 
-                two_gram_one = two_gram_one + int(n_gram[2]) #column 3 gives us "match count"
-                two_gram_two = two_gram_two + int(n_gram[3]) #column 4 gives us "volume count"
+                n_gram = navy_ship.split("\t") #separate the string so we can focus on what we care about 
+                two_gram_one = two_gram_one + int(n_gram[2]) #go through each record and add up all the match counts and volume counts
+                two_gram_two = two_gram_two + int(n_gram[3]) 
             for record in z.search(start=word1_capital + b" " + word2_capital + b"\t1980", stop=word1_capital + b" " + word2_capital + b"\t2009"): #accounting for capitalization
                 navy_ship = record.decode("utf-8")
                 n_gram = navy_ship.split("\t")   
-                two_gram_one = two_gram_one + int(n_gram[2])
+                two_gram_one = two_gram_one + int(n_gram[2]) 
                 two_gram_two = two_gram_two + int(n_gram[3])
             for record in z.search(start=word1_capital + b" " + word2 + b"\t1980", stop=word1_capital + b" " + word2 + b"\t2009"): #accounting for capitalization
                 navy_ship = record.decode("utf-8")
@@ -78,7 +80,7 @@ if __name__ == '__main__':
                 navy = record.decode("utf-8")
                 one_gram = navy.split("\t")
                 if int(one_gram[1]) >= 1980:
-                    one_gram_one = one_gram_one +int(one_gram[2]) #match count
+                    one_gram_one = one_gram_one +int(one_gram[2]) #loop through and add up all the match counts for each word
                     one_gram_two = one_gram_two +int(one_gram[3]) #volume count
             for record in z_1gram.search(prefix=word1_capital):  #need to account for capitalization for this as well
                 navy = record.decode("utf-8")
@@ -105,9 +107,9 @@ if __name__ == '__main__':
             
             #count((count(gun rights)/count(gun)) / count(gun) - count(gun rights) / count(gun) --> this is the odds ratio
         
-            if one_gram_one != 0:
+            if one_gram_one != 0: #to avoid divide by zero errors
                 P_value = (two_gram_one / one_gram_one)
-                oddsratio_col1 = (two_gram_one / one_gram_one) / ((one_gram_one - two_gram_one) / one_gram_one)
+                oddsratio_col1 = (two_gram_one / one_gram_one) / ((one_gram_one - two_gram_one) / one_gram_one) # P / (1-P)
                 oddsratio_col2 = (two_gram_two / one_gram_two) / ((one_gram_two - two_gram_two) / one_gram_two)
                 deltaP_col1 = (two_gram_one / one_gram_one) - ((n2_count_one - two_gram_one) / (corpus_size - one_gram_one))
                 deltaP_col2 = (two_gram_two / one_gram_two) - ((n2_count_two - n2_count_two) / (corpus_size - one_gram_two))
@@ -124,7 +126,7 @@ if __name__ == '__main__':
                 return(word1.decode('utf-8'), word2.decode('utf-8'), two_gram_one, two_gram_two, one_gram_one, one_gram_two, "NA", "NA", "NA", "NA", "NA", "NA", "NA", "NA", "NA", "NA")
 
     if __name__ == '__main__':
-        with open('D:\PhD Stuff\Linguistics Stuff\Google NGRAMS\outputforngrams.csv', newline='') as csvfile_input:
+        with open('D:\PhD Stuff\Linguistics Stuff\Google NGRAMS\outputforngrams.csv', newline='') as csvfile_input: #Read CSV with pairs of words, output match count, volume count, P value, odds ratio, etc
             with open('D:\PhD Stuff\Linguistics Stuff\Google NGRAMS\words for oddsratio calculations exp4.csv', 'w') as csvfile_output:
                 my_two_grams_reader = csv.reader(csvfile_input, delimiter=',')
                 my_two_grams_writer = csv.writer(csvfile_output, delimiter=',', lineterminator='\n')
